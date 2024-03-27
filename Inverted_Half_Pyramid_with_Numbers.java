9a0a86122a1fcdd4071f9924736587d04a2e8963
Parth Rajyagor <98021534+parth-rajyagor@users.noreply.github.com>
2024-03-27 13:34:49 +0530

    Create Inverted_Half_Pyramid_with_Numbers.java

---
+++ Inverted_Half_Pyramid_with_Numbers.java
@@ -0,0 +1,27 @@
+/* WAP in java to print inverted half pyramid with numbers
+
+    12345
+    1234
+    123
+    12
+    1
+*/
+
+import java.util.Scanner;
+
+public class Inverted_Half_Pyramid_with_Numbers
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        for(int i=a;i>0;i--)
+        {
+            for(int j=i;j>0;j--)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
