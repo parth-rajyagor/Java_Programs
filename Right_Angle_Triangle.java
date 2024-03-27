a38b34c39f644797523f2a343042877f40115d05
Parth Rajyagor <98021534+parth-rajyagor@users.noreply.github.com>
2024-03-27 13:13:41 +0530

    Create Right_Angle_Triangle.java

---
+++ Right_Angle_Triangle.java
@@ -0,0 +1,26 @@
+/* WAP in java to print right angle triangle/ half pyramid star pattern
+    *
+    **
+    ***
+    ****
+    *****
+*/
+
+import java.util.Scanner;
+
+public class Right_Angle_Triangle
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        for(int i=1;i<=a;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
