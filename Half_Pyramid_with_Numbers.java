3e2ae7f3104a8169f7077b9d54dcb5d6c8d1a9bd
Parth Rajyagor <98021534+parth-rajyagor@users.noreply.github.com>
2024-03-27 13:22:05 +0530

    Create Half_Pyramid_with_Numbers.java

---
+++ Half_Pyramid_with_Numbers.java
@@ -0,0 +1,25 @@
+/* WAP in java to print half pyramid with numbers
+    1
+    12
+    123
+    1234
+*/
+
+import java.util.Scanner;
+
+public class Half_Pyramid_with_Numbers
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        for(int i=1;i<=a;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
