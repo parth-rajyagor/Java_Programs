0ed2067d43eaa660dcd6031eb69349e4aff8f013
Parth Rajyagor <98021534+parth-rajyagor@users.noreply.github.com>
2024-03-30 12:08:11 +0530

    Update Solid_Rhombus.java

--- Solid_Rhombus.java
+++ Solid_Rhombus.java
@@ -29,3 +29,24 @@ public class Solid_Rhombus
         }
     }
 }
+
+// 2nd Method
+public class Solid_Rhombus
+{
+    public static void main(String[] args)
+    {
+        int a = 5;
+        for(int i=1;i<=a;i++)
+        {
+            for(int j=a-i;j>0;j--)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=a;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
